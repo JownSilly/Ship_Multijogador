6ade6b359b264a236c0dca06ba51e5f9d59d8709
JownSilly <joaovitordealmeidaojeda2@hotmail.com>
2023-10-25 01:35:38 -0400

    Organizando WorkSpace

---
+++ Assets/Script/PlayerInputHandler.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerInputHandler : MonoBehaviour
+{
+    public Vector2 movementInput;
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
+
+}
