533113da25efe85fb672157a6c0d17243f21261c
JownSilly <joaovitordealmeidaojeda2@hotmail.com>
2023-11-02 13:17:20 -0400

    PlayerMovement -Multipplayer - Working

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,77 +1,56 @@
 using UnityEngine;
 using Unity.Netcode;
 using UnityEngine.InputSystem;
-
 public class PlayerController : NetworkBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float rotationSpeed = 180f;
-    [SerializeField] private float decelerationRate = 2f;
-    private Vector2 _moveDirection;
-    private PlayerInputHandler inputHandler;
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 100f;
+    private PlayerInputHandler _inputHandler;
     private Rigidbody2D rb;
-    private float movementInputValue;
-    private float currentSpeed;
+    private Vector2 _moveDirection;
 
-    public override void OnNetworkSpawn()
-    {
-        if (!IsOwner) gameObject.GetComponent<PlayerInput>().enabled = false;
-    }
     private void Start()
     {
-        
         rb = GetComponent<Rigidbody2D>();
         _moveDirection = Vector2.zero;
-        inputHandler = GetComponent<PlayerInputHandler>();
-        
-        
+        if (IsLocalPlayer)
+        {
+            GetComponent<PlayerInput>().enabled = true;
+            _inputHandler = GetComponent<PlayerInputHandler>();
+        }
     }
-
+    void Update()
+    {
+        if (IsClient)
+            if (!IsLocalPlayer) return;
+            if (_inputHandler.movementInput != _moveDirection)
+            _moveDirection = _inputHandler.movementInput;
+            MoveServerRpc(_moveDirection);
+    }
     [ServerRpc]
     public void MoveServerRpc(Vector2 move)
     {
         _moveDirection = move.normalized;
     }
-
     private void FixedUpdate()
     {
-        if(IsClient)
-        Debug.Log("cLIENT" + inputHandler.movementInput);
-        if (IsHost)
+        if (IsServer)
         {
-            Debug.Log("HOST" + inputHandler.movementInput);
+            float moveInput = -_moveDirection.y;
+            float rotateInput = _moveDirection.x;
+
+            // Rotacionar o barco
+            if (rotateInput != 0)
+            {
+
+                float rotation = rotateInput * rotationSpeed * Time.fixedDeltaTime;
+                transform.Rotate(0, 0, -rotation);
+            }
+
+            // Mover o barco para frente ou para trás
+            Vector2 movement = transform.up * moveInput * moveSpeed * Time.fixedDeltaTime;
+            rb.MovePosition(rb.position + movement);
         }
         
-        movementInputValue = -inputHandler.movementInput.y;
-        UpdateSpeed();
-
-            
-            Move();
-            Turn();
-    }
-
-    private void UpdateSpeed()
-    {
-        if (movementInputValue == 0 && currentSpeed != 0)
-        {
-            currentSpeed = Mathf.MoveTowards(currentSpeed, 0, decelerationRate * Time.deltaTime);
-        }
-        else
-        {
-            currentSpeed = Mathf.MoveTowards(currentSpeed, moveSpeed * movementInputValue, Time.deltaTime * (moveSpeed / 2));
-        }
-    }
-
-    private void Move()
-    {
-        Vector2 movement = transform.up * currentSpeed;
-        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
-    }
-
-    private void Turn()
-    {
-        float turn = inputHandler.movementInput.x;
-        float turnAngle = -turn * rotationSpeed * Time.fixedDeltaTime;
-        rb.MoveRotation(rb.rotation + turnAngle);
     }
 }
